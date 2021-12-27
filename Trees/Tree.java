aa1fa0420ba030493834805b1e8fb8f7539f8ba5
Abhijeet Vishwakarma <vishwakarma2003@hotmail.com>
2021-12-27 23:39:03 +0530

    Added more functions in Tree Program

--- Trees/Tree.java
+++ Trees/Tree.java
@@ -28,15 +28,45 @@ class Tree
         //Preorder traversal
         System.out.println("Preorder Traversal");
         obj.preorder(root);
-        
+        System.out.println("\nPost Order traversal");
+        obj.postorder(root);
+        System.out.println("\n Inorder Traversal");
+        obj.inorder(root);
+        System.out.println("\nTotal no. of Nodes = "+obj.countnode(root));
+        System.out.println("\nTotal no. of leaf nodes"); //to be solved
     }
 
     void preorder(Node root)
-    {
+    {   
+        //termination condition  [ root left right ]
         if(root == null)
             return;
         System.out.print(root.data+" ");
         preorder(root.left);
         preorder(root.right);
+    }
+
+    void postorder(Node root)
+    {
+        //left root right
+        if(root == null)return;
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data+" ");
+    }
+
+    void inorder(Node root)
+    {
+        //left root right
+        if(root == null)return;
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    
+    int countnode(Node root)
+    {
+        if(root == null)return 0;
+        return ( 1 + countnode(root.left) + countnode(root.right));
     }
 }
