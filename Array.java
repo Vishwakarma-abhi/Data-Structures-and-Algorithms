945dbcef36de64536118ad120c57fc2a8a93d7b4
Abhijeet Vishwakarma <vishwakarma2003@hotmail.com>
2021-12-27 13:11:25 +0530

    New Files Created

--- Array.java
+++ Array.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 import java.io.*;
@@ -13,17 +14,17 @@ class Array
         {
             arr[i]=sc.nextInt();
         }
-        System.out.println("Array before deletion");
+        System.out.println("our Array");
         for (int i=0;i<5;i++)
         {
            System.out.println(arr[i]);
         }
-
+        /*
         //Deleting Element at some position
         System.out.println("Enter the positoin of the element You want to delete");
         int pos=sc.nextInt();
         int len=arr.length;
-        if(pos >len || pos <=0)
+        if(pos >len || pos <0)
         {
             System.out.println("Invalid");
         }
@@ -33,14 +34,37 @@ class Array
             {
                 arr[i]=arr[i+1];
             }
-            len--;
         }
+        len--;
         
         System.out.println("Arraya after deletion");
         for (int i=0;i<len;i++)
         {
             System.out.println(arr[i]);
         }
+        */
+
+        //Approach1 O(n) largest element
+
+        int max=0,index=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+                index=i;
+            }
+        }
+        System.out.println("largest Element of the array:"+max);
+        System.out.println("Index of Largest Element is "+index);
+
+        //Second Largest Element
+        Arrays.sort(arr);
+        System.out.println("Second Largest element is : "+arr[arr.length-2]);
+
+
+
+
         
             
 
