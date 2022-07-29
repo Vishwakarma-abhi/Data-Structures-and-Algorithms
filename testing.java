327a0ed7a17fe9535d385bebfe84b5595edf4519
Abhijeet Vishwakarma <vishwakarma2003@hotmail.com>
2022-07-29 23:23:33 +0530

    New

---
+++ testing.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+        ArrayList<Integer> list = new ArrayList<>();
+		
+		for(int i = 0 ; i  < 4 ; i++)
+		{
+		    list.put(i);
+		}
+		System.out.println(list);
+    }
+    
+}
