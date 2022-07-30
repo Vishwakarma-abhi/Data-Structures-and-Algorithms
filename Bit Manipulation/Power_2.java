8c77738191baecd320c831f69df0fc17b2c68b75
Abhijeet Vishwakarma <vishwakarma2003@hotmail.com>
2022-07-31 00:48:42 +0530

    Modified Power of Two Program

--- Bit Manipulation/Power_2.java
+++ Bit Manipulation/Power_2.java
@@ -1,42 +1,30 @@
 import java.util.*;
+
 public class Power_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter a number");
         int n = sc.nextInt();
-        
-        
+
         boolean res = check_power(n);
-        if(res)
-        {
+        if (res) {
             System.out.println("Power of Two");
-        }
-        else
-        {
+        } else {
             System.out.println("Not a Power of Two");
         }
-        
+
     }
 
-    static boolean check_power(int n)
-    {
-        //Base Case
-        if(n == 0)
-        {
+    static boolean check_power(int n) {
+        // Base Case
+        if (n <= 0) {
+            return false;
+        } else if ((n & (n - 1)) == 0) { // Brian algorithm concept
+            return true;
+        } else {
             return false;
         }
-
-        while(n != 1 )
-        {
-            if(n % 2 != 0)
-            {
-                return false;
-                
-            }
-            n = n / 2;
-        }
-        return true;
     }
-    
+
 }
