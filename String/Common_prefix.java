53ccd58e567e3b9cc19d89205537dcd61462cfe3
Abhijeet Vishwakarma <vishwakarma2003@hotmail.com>
2022-11-26 00:47:23 +0530

    Common Prefix File

---
+++ String/Common_prefix.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Common_prefix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String words[] = { "flower", "flow", "flight" };
+        ArrayList<String> prefix_ArrayList = new ArrayList<>();
+
+        String common_prefix = "";
+
+        // System.out.println(calculate_prefix(words[0], words[1]));
+
+        for (int i = 0; i < words.length - 1; i++) {
+            prefix_ArrayList.add(calculate_prefix(words[i], words[i + 1]));
+
+        }
+        System.out.println(prefix_ArrayList);
+
+    }
+
+    public static String calculate_prefix(String str1, String str2) {
+
+        String prefix = "";
+
+        int l = Math.min(str1.length(), str2.length());
+        int i = 0, j = 0;
+        while (l != 0) {
+            if (str1.charAt(i) == str2.charAt(j)) {
+                prefix += str1.charAt(i);
+
+            }
+            i++;
+            j++;
+            l--;
+
+        }
+
+        return prefix;
+
+    }
+}
---
+++ String/tempCodeRunnerFile.java
@@ -0,0 +1,1 @@
+System.out.println(prefix_ArrayList);
