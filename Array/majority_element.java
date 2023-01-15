bbf692301f5e71be1406283c0bb69a24cfbf2e45
ABHIJEET VISHWAKARMA <96198653+Vishwakarma-abhi@users.noreply.github.com>
2023-01-15 22:31:18 +0530

    Majority Element

--- Array/majority_element.java
+++ Array/majority_element.java
@@ -7,37 +7,49 @@ public class majority_element {
         // we need to optimize the code compulsory
 
         // Input Array
-        int arr[] = { 8, 8, 7, 7, 7 };
+        int arr[] = { 1, 2, 3 };
         int len = arr.length;
 
         // naive approach
         // Time complexity -> O(size of the map) -> O(n)
         // Space complexity -> O(n)
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            map.put(arr[i], i);
-        }
-
-        naive_Approach(arr, map, len);
+        System.out.println("Majority Element --> " + majorityElement(arr, len));
 
     }
 
-    public static void naive_Approach(int[] arr, HashMap<Integer, Integer> map, int len) {
-        int maj = 0;
-        int count = 0;
-        for (Integer i : map.keySet()) {
-            count = 0;
-            for (int j = 0; j < arr.length; j++) {
-                if (i == arr[j])
-                    count++;
+    static int majorityElement(int a[], int size) {
+        // your code here
+
+        if (size == 1)
+            return a[0];
+
+        // sort the elements so that all the duplicates are aligned in adjacent manner
+        Arrays.sort(a);
+
+        // counter == 1 because all the elements will be atleast once in the array
+        int count = 1;
+        // (size-1) so that a[i+1] adjacent elements are compared
+        for (int i = 0; i < size - 1; i++) {
+            // since array is sorted so we are comparing the adjacent elements
+            // if the adjacent elements are equal it means it is occuring more than once and
+            // counter is increamented
+            if (a[i] == a[i + 1]) {
+                count++;
             }
-            System.out.println(i + " -> " + count);
-            if (count > (len / 2)) {
-                maj = i;
-                break;
+            // else the adjacent element is not equal and counter for a[i] will be one
+            else {
+                count = 1;
+            }
+            // so if the count element is greater than size / 2
+            // we found the majority element
+            if (count > size / 2) {
+                return a[i];
+
             }
         }
-        System.out.println("Majority Element --> " + maj);
+
+        // if no majority element is present we return -1
+        return -1;
     }
 
 }
