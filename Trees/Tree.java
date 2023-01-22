0688eed89f8cb1d8a0f3d60fd3f3c8bbc807e833
ABHIJEET VISHWAKARMA <96198653+Vishwakarma-abhi@users.noreply.github.com>
2023-01-22 23:49:32 +0530

    updated

--- Trees/Tree.java
+++ Trees/Tree.java
@@ -1,102 +1,91 @@
 
 import java.util.*;
 
+public class Tree {
 
-public class Tree
-{
-        
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
- 
         Node root = create();
-        
-        //Preorder traversal
+
+        // Preorder traversal
         System.out.println("\nPreorder Traversal");
         preorder(root);
 
         System.out.println();
-        //PostOrder traversal
+        // PostOrder traversal
         System.out.println("\nPost Order traversal");
         postorder(root);
         System.out.println();
-        //Inprder traversal
+        // Inprder traversal
         System.out.println("\n Inorder Traversal");
         inorder(root);
-    
-   }
 
-    //creating and taking user input for the tree
-    static Node create()
-    {
+    }
+
+    // creating and taking user input for the tree
+    static Node create() {
         Node root = null;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Data in the node ");
         int value = sc.nextInt();
 
-        if(value == -1)
-        {
+        if (value == -1) {
             return null;
         }
         root = new Node(value);
-        
-        System.out.println("Enter the left child of "+ value);
-        root.left=create();
-        
-        System.out.println("Enter the right child of"+value);
-        root.right=create();
+
+        System.out.println("Enter the left child of " + value);
+        root.left = create();
+
+        System.out.println("Enter the right child of" + value);
+        root.right = create();
 
         return root;
 
-        
-
-
     }
-    static void preorder(Node root)
-    {   
-        //termination condition  [ root left right ]
-        if(root == null)
+
+    static void preorder(Node root) {
+        // termination condition [ root left right ]
+        if (root == null)
             return;
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         preorder(root.left);
         preorder(root.right);
     }
 
-    static void postorder(Node root)
-    {
-        //left root right
-        if(root == null)return;
+    static void postorder(Node root) {
+        // left root right
+        if (root == null)
+            return;
         postorder(root.left);
         postorder(root.right);
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
     }
 
-    static void inorder(Node root)
-    {
-        //left root right
-        if(root == null)return;
+    static void inorder(Node root) {
+        // left root right
+        if (root == null)
+            return;
         inorder(root.left);
-        System.out.print(root.data+" ");
+        System.out.print(root.data + " ");
         inorder(root.right);
     }
-    
-    int countnode(Node root)
-    {
-        if(root == null)return 0;
-        return ( 1 + countnode(root.left) + countnode(root.right));
+
+    int countnode(Node root) {
+        if (root == null)
+            return 0;
+        return (1 + countnode(root.left) + countnode(root.right));
     }
 }
 
-
-class Node
-{
+class Node {
     int data;
     Node left;
     Node right;
 
-    public Node(int data)
-    {
-        this.data=data;
-        
+    public Node(int data) {
+        this.data = data;
+
     }
 }
