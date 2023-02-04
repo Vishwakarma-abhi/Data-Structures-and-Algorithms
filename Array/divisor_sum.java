36a5a58c13e852696015277a0f03ebff31f44929
ABHIJEET VISHWAKARMA <96198653+Vishwakarma-abhi@users.noreply.github.com>
2023-02-04 23:11:51 +0530

    O(n)

---
+++ Array/divisor_sum.java
@@ -0,0 +1,43 @@
+
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class divisor_sum {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int N = sc.nextInt();
+
+            Solution ob = new Solution();
+            long ans = ob.sumOfDivisors(N);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+// User function Template for Java
+class Solution {
+    static long sumOfDivisors(int N) {
+        // code here
+
+        long sum = 0, fsum = 0;
+
+        for (int i = 1; i <= N + 1; i++) {
+            sum = 0;
+            for (int j = 1; j <= N + 1; j++) {
+                // if j is a divisor then add it
+                if (i % j == 0) {
+                    sum += i;
+                }
+            }
+            fsum = sum;
+
+        }
+        return fsum;
+    }
+}
