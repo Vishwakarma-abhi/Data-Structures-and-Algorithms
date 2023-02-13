b98b4c4bc536762a4fee74d049e6f7bf72d20447
ABHIJEET VISHWAKARMA <96198653+Vishwakarma-abhi@users.noreply.github.com>
2023-02-13 11:19:28 +0530

    O(n) second largest

--- Array/secondLargest.java
+++ Array/secondLargest.java
@@ -9,24 +9,17 @@ public class secondLargest {
 
         // In this Question We need to find the second largest element in the array
 
-        // Approach -> We can calculate the difference of maximum element with all the
-        // elements in the array
-        // The element with the minimum difference and not equal to zero will be the
-        // second largest since
-        // it will be nearest to maximum element of the array
-
-        // here we get the maximum element of the array
-
         int max = get_max(arr, n);
 
         // now we will traverse and check for minDifference and also store that element
-        int minDifference = Integer.MAX_VALUE, second_max = 0;
-        for (int i = 0; i < 0; i++) {
-
-            // first we calculate for min difference
-
+        int second_max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > second_max && arr[i] != max) {
+                second_max = arr[i];
+            }
         }
-    }
+       
+        System.out.println("Second Max -> " + second_max);
 
     }
 
