ed4e7522b5c5279b659b94c541a867978ef924d7
ABHIJEET VISHWAKARMA <96198653+Vishwakarma-abhi@users.noreply.github.com>
2023-02-18 00:12:39 +0530

    Union

--- Array/union.java
+++ Array/union.java
@@ -1,15 +1,56 @@
 import java.util.*;
 
 public class union {
+    // Union or Merging Two sorted Array Using Two Pointers
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         // Time -> O(n+m) Space -> O(n);
         System.out.println("program for Union two arrays");
         int arr1[] = { 1, 2, 3, 4, 5 };
-        int arr2[] = { 2, 3, 4 };
-        findUnion(arr1, arr2, 5, 3);
+        int arr2[] = { 2, 3, 7 };
+        // findUnion(arr1, arr2, 5, 3);
+        Union(arr1, arr2, 5, 3);
 
+    }
+
+    public static void Union(int arr1[], int arr2[], int n, int m) {
+
+        // two pointer appraoch
+        int ptr1 = 0, ptr2 = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (ptr1 < n && ptr2 < m) {
+
+            // case1
+            if (arr1[ptr1] == arr2[ptr2]) {
+                // then add any one
+                list.add(arr1[ptr1]);
+                ptr1++;
+
+            } else if (arr1[ptr1] > arr2[ptr2]) {
+                // then add arr2[ptr2]
+                list.add(arr2[ptr2]);
+                ptr2++;
+            } else {
+                // then add arr1[ptr1]
+                list.add(arr1[ptr1]);
+                ptr1++;
+
+            }
+
+        }
+        while (ptr1 != n) {
+            list.add(arr1[ptr1]);
+            ptr1++;
+        }
+        while (ptr2 != m) {
+            list.add(arr2[ptr2]);
+            ptr2++;
+
+        }
+        System.out.println(list);
     }
 
     // Function to return a list containing the union of the two arrays.
