884a766970f7d3b6592e2631a4a8b5d553f68bd2
ABHIJEET VISHWAKARMA <96198653+Vishwakarma-abhi@users.noreply.github.com>
2023-03-17 20:20:41 +0530

    optimized-O(n)

--- Array/RainWaterTrapping.java
+++ Array/RainWaterTrapping.java
@@ -2,13 +2,47 @@ import java.util.*;
 
 public class RainWaterTrapping {
 
-    // Brute force
-    // Time - O(n^2) Space - O(n)
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int arr[] = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
 
+        // Brute force
+        // Time - O(n^2) Space - O(n)
+        BruteForce(arr);
+
+        // Optimized Approach
+        approach2(arr);
+
+    }
+
+    private static void approach2(int[] arr) {
+
+        // In this approach we precalculate the left max and right max for each array
+        // element
+        int size = arr.length;
+        // we will create a left array and right array
+        int left[] = new int[size];
+        left[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            left[i] = Math.max(arr[i], left[i - 1]);
+        }
+
+        // similarly calculate the right max array
+        int right[] = new int[size];
+        right[size - 1] = arr[size - 1];
+        for (int i = size - 2; i >= 0; i--) {
+            right[i] = Math.max(arr[i], right[i + 1]);
+        }
+        int water = 0;
+        // now we will find the amount of water for every building
+        for (int i = 0; i < size; i++) {
+            water += Math.min(left[i], right[i]) - arr[i];
+        }
+        System.out.println("Total Rainwater Trapped " + water);
+    }
+
+    public static void BruteForce(int[] arr) {
         int water = 0;
         for (int i = 1; i < arr.length - 1; i++) {
 
@@ -25,10 +59,12 @@ public class RainWaterTrapping {
                 right = Math.max(right, arr[j]);
             }
 
+            // main Trick formula to be observed and calculated
             water += Math.min(left, right) - arr[i];
 
         }
         System.out.println("Total trapped water => " + water);
+
     }
 
 }
