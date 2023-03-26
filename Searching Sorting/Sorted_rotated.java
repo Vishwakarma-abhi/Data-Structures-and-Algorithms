0c3caed0fc88b90e947320ca4019609fc3a690b5
ABHIJEET VISHWAKARMA <96198653+Vishwakarma-abhi@users.noreply.github.com>
2023-03-26 23:55:37 +0530

    modified

--- Searching Sorting/Sorted_rotated.java
+++ Searching Sorting/Sorted_rotated.java
@@ -1,9 +1,11 @@
+import java.lang.Thread.State;
 import java.util.*;
 
+// time - O(logn)
 public class Sorted_rotated {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int arr[] = { 102, 110, 2, 3, 4, 6, 12, 45, 99 };
+        int arr[] = { 4, 5, 6, 7, 0, 1, 2 };
 
         int start = 0, end = arr.length - 1;
         int idx = 0;
@@ -30,5 +32,46 @@ public class Sorted_rotated {
             }
         }
         System.out.println("Index -> " + idx);
+
+        int target = 0;
+
+        int part1 = binsearch(arr, 0, idx - 1, target);
+        int part2 = binsearch(arr, idx, end, target);
+
+        if (part1 == 1) {
+            System.out.println(target + "  found");
+        } else if (part2 == 1) {
+            System.out.println(target + "  Found");
+        }
+
+        else {
+            System.out.println("Not found");
+        }
+
     }
+
+    private static int binsearch(int[] arr, int i, int j, int target) {
+
+        int start = 0, end = arr.length - 1;
+        int idx = 0;
+        int size = arr.length;
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return 1;
+            }
+
+            if (arr[mid] < target) {
+                start = mid + 1;
+            }
+            if (arr[mid] > target) {
+                end = mid - 1;
+            }
+        }
+
+        return 0;
+    }
+
 }
