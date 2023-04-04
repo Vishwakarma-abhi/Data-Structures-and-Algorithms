d51fbb2e799e9660c36f3b1d79110d6f5b590131
ABHIJEET VISHWAKARMA <96198653+Vishwakarma-abhi@users.noreply.github.com>
2023-04-04 23:56:16 +0530

    infinite search

---
+++ Searching Sorting/Infinite_search.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+public class Infinite_search {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int low = 0, high = 1;
+
+        // Since it is an infinite Sorted Array SO we can apply Binary Search
+        // But Before that we need High value because the array is infinite
+        System.out.println("Enter target value to be searched");
+        int target = sc.nextInt();
+        while (target > arr[high]) {
+            low = high;
+            high = high * 2;
+        }
+
+        // Now We can apply the binary search
+        int res = binsearch(arr, 0, high,  target);
+        if (res == -1) {
+            System.out.println("Not found");
+        } else {
+            System.out.println("Found");
+        }
+
+    }
+
+    private static int binsearch(int[] arr, int low, int high, int target) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+            if (arr[mid] < target) {
+                low = mid + 1;
+
+            } else {
+                high = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+
+}
