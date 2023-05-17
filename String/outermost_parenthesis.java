f9adcfbf76503c837a3ceb7176031cf963dd3517
ABHIJEET VISHWAKARMA <96198653+Vishwakarma-abhi@users.noreply.github.com>
2023-05-17 23:50:36 +0530

    using stack

--- String/outermost_parenthesis.java
+++ String/outermost_parenthesis.java
@@ -1,3 +1,33 @@
+import java.util.*;
+
 public class outermost_parenthesis {
+    public static void main(String[] args) {
+        String str = "(()())(())";
+
+        // Stack for tracking theBrackets
+        Stack<Character> stk = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        // ===> (()())(())
+        for (int i = 0; i < str.length(); i++) {
+            // ( 
+            if (str.charAt(i) == '(') {
+                if (stk.size() > 0) {
+                    sb.append(str.charAt(i));
+
+                }
+                stk.push(str.charAt(i));
+            } else {
+                stk.pop();
+
+                if (stk.size() > 0) {
+                    sb.append(str.charAt(i));
+
+                }
+
+            }
+        }
+        System.out.println(sb.toString());
+
+    }
 
 }
