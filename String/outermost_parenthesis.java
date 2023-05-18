8b8401f3eafa372de17a95f03519a39ce5f34c22
ABHIJEET VISHWAKARMA <96198653+Vishwakarma-abhi@users.noreply.github.com>
2023-05-18 07:51:33 +0530

    Stack concept

--- String/outermost_parenthesis.java
+++ String/outermost_parenthesis.java
@@ -4,29 +4,45 @@ public class outermost_parenthesis {
     public static void main(String[] args) {
         String str = "(()())(())";
 
+        System.out.println("Given String ");
+        System.out.println(str);
         // Stack for tracking theBrackets
-        Stack<Character> stk = new Stack<>();
-        StringBuilder sb = new StringBuilder();
-        // ===> (()())(())
+        Stack<Character> brackets = new Stack<>();
+        // variable to store answer
+        String ans = "";
+
         for (int i = 0; i < str.length(); i++) {
-            // ( 
+
+            // now we will check if it is open "(" bracket
             if (str.charAt(i) == '(') {
-                if (stk.size() > 0) {
-                    sb.append(str.charAt(i));
-
+                // now we will check if the open bracket is outermost or not
+                // if the size of the stack is > 0 it means It is having some element and the
+                // current processing ith character is not outermost
+                // so include it in the answer
+                if (brackets.size() > 0) {
+                    ans += str.charAt(i);
                 }
-                stk.push(str.charAt(i));
+                // also push it in the stack
+                // and when it will encounter close it will be removed
+                brackets.push(str.charAt(i));
             } else {
-                stk.pop();
-
-                if (stk.size() > 0) {
-                    sb.append(str.charAt(i));
-
+                // now if it is a close bracket
+                // now we will first remove and then check condition
+                // since it encounters its pair it is removed
+                brackets.pop();
+                // now we will check if after removing current stack brackets
+                // whether it was outermost or not -> stk.size() == 0 if yes then it is
+                // outermost
+                // then we should not add it to answer
+                if (brackets.size() > 0) {
+                    // then the removed bracket was outermost so donot add it otherwise add it
+                    ans += str.charAt(i);
                 }
 
             }
         }
-        System.out.println(sb.toString());
+
+        System.out.println("After Removing Outermost Parenthesis  " + ans);
 
     }
 
