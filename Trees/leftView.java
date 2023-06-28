f4c16686a95de0f2b84d61ef39855a79763a1335
ABHIJEET VISHWAKARMA <96198653+Vishwakarma-abhi@users.noreply.github.com>
2023-06-28 23:10:36 +0530

    left-right view Tree

---
+++ Trees/leftView.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class leftView {
+
+    static Scanner sc = null;
+
+    public static void main(String[] args) {
+        sc = new Scanner(System.in);
+
+        Node root = createTree();
+        // List<List<Integer>> list = new ArrayList<>();
+        // list.add(new ArrayList<Integer>(root.data));
+        ArrayList<Integer> list = new ArrayList<>();
+        printLeftView(root, list, 0);
+        System.out.println(list);
+    }
+
+    private static void printLeftView(Node root, ArrayList<Integer> list, int level) {
+
+        if (root == null)
+            return;
+        if (level == list.size()) {
+            list.add(root.data);
+        }
+        printLeftView(root.left, list, level + 1);
+        printLeftView(root.right, list, level + 1);
+
+    }
+
+    private static Node createTree() {
+
+        Node root = null;
+        System.out.println("Enter data: ");
+        int data = sc.nextInt();
+
+        if (data == -1)
+            return null;
+
+        root = new Node(data);
+
+        System.out.println("Enter left for " + data);
+        root.left = createTree();
+
+        System.out.println("Enter right for " + data);
+        root.right = createTree();
+
+        return root;
+    }
+}
+
+class Node {
+    Node left, right;
+    int data;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
