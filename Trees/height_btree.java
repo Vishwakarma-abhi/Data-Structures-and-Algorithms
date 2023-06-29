d7c1e4eaecbfa0f6e5b1cd4975a20385b4e3c0ab
ABHIJEET VISHWAKARMA <96198653+Vishwakarma-abhi@users.noreply.github.com>
2023-06-29 23:27:00 +0530

    height

--- Trees/height_btree.java
+++ Trees/height_btree.java
@@ -7,56 +7,61 @@ public class height_btree {
 
         int height = height_tree(root);
 
-        System.out.println("Height of the tree = "+ height);
+        System.out.println("Height of the tree = " + height);
     }
 
-    static int height_tree(Node root)
-    {
-        if(root == null)
-        {
+    static int height_tree(Node root) {
+        /*
+         * 
+         * Approach - 1
+         * Height of the Tree == Maximum Depth
+         * 
+         * Here We can relate the depth with Maximum depth of either Left subtree or
+         * right subtree
+         * 
+         * so we will choose the maximum height (depth ) between the subtrees
+         * 
+         * 
+         * Approach - 2
+         * - Traverse the tree in Level wise
+         * - Calculate the number of levels
+         */
+        if (root == null) {
             return 0;
         }
-        return Math.max(1 + height_tree(root.left),1 + height_tree(root.right));
+        return Math.max(1 + height_tree(root.left), 1 + height_tree(root.right));
 
     }
-    
-    
-    
-    static Node create()
-    {
-        Node root  =  null;
+
+    static Node create() {
+        Node root = null;
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the data");
         int value = sc.nextInt();
 
-        if(value == -1)
-        {
+        if (value == -1) {
             return null;
         }
-        root =  new Node(value);
+        root = new Node(value);
 
-        System.out.println("Enter the left child of  "+ value);
+        System.out.println("Enter the left child of  " + value);
         root.left = create();
 
-        System.out.println("Enter the right child of  "+ value);
+        System.out.println("Enter the right child of  " + value);
         root.right = create();
 
         return root;
-        
 
     }
 
-
-    static class Node{
+    static class Node {
         int data;
-        Node left,right;
+        Node left, right;
 
-        public Node(int data)
-        {
+        public Node(int data) {
             this.data = data;
         }
     }
 
-    
 }
